8ee232680b070f3ad6eef00cabfa52eb29ba04dc
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-15 12:05:53 +0200

    step: add panel image generator adapter

--- infra/adapters/panel/index.ts
+++ infra/adapters/panel/index.ts
@@ -1,4 +1,5 @@
 import type { EnvironmentResultType } from "+infra/env";
+import { createPanelImageGenerator } from "./panel-image-generator.adapter";
 import { createPanelTemplateGenerator } from "./panel-template-generator.adapter";
 import { createWeatherCurrentReader } from "./weather-current-reader.adapter";
 
@@ -6,5 +7,6 @@ export async function createPanlAdapters(Env: EnvironmentResultType) {
   return {
     WeatherCurrentReader: createWeatherCurrentReader(Env),
     PanelTemplateGenerator: createPanelTemplateGenerator(Env),
+    PanelImageGenerator: createPanelImageGenerator(Env),
   };
 }
