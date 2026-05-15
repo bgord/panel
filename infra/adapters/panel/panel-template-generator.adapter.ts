685b4260fd9786617e4da2c3614ad1592bf840dd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-15 16:39:26 +0200

    step: improve template

--- infra/adapters/panel/panel-template-generator.adapter.ts
+++ infra/adapters/panel/panel-template-generator.adapter.ts
@@ -8,6 +8,8 @@ type Dependencies = { FileReaderText: bg.FileReaderTextPort };
 const template = /* HTML */ (weather: Panel.Ports.Weather, css: string) => `
     <html>
       <head>
+        <meta charset="utf-8"  />
+        <meta name="viewport" content="width=device-width, initial-scale=1"  />
         <style>
           html {
             font-family: sans-serif;
@@ -16,23 +18,34 @@ const template = /* HTML */ (weather: Panel.Ports.Weather, css: string) => `
         </style>
       </head>
       <body data-bg="neutral-100">
-        <h1>Panel</h1>
-        <section>
-          <h2>Pogoda</h2>
-          <div>${weather.condition}</div>
-          <img src="${weather.conditionImageUrl}" alt="${weather.condition}" />
-          <strong>${weather.generatedAt
+        <h1 data-fs="xl" data-color="neutral-800" data-bg="neutral-300" data-p="3">Panel</h1>
+
+        <section data-fs="lg" data-color="neutral-700" data-bg="neutral-200" data-p="3">
+          ${weather.generatedAt
             .toInstant()
             .toZonedDateTimeISO("Europe/Warsaw")
             .toLocaleString("pl-PL", { day: "numeric", month: "long" })}
-          </strong>
-          <div>${weather.temperatureCelsius} °C [${weather.feelsLikeCelsius} °C]</div>
-          <small>${weather.location}</small>
-
-          <div>${weather.precipitation.probability}% opadów (ok. ${weather.precipitation.hour}:00)</div>
         </section>
 
-        <section>
+        <section data-stack="y" data-cross="center" data-p="3">
+          <div data-fs="5xl" data-color="neutral-600" data-my="8">${weather.location}</div>
+
+          <div data-stack="x" data-gap="12" data-cross="end">
+            <div data-stack="y">
+              <img src="${weather.conditionImageUrl}" alt="${weather.condition}" />
+              <div data-fs="xl" data-color="neutral-400">${weather.condition}</div>
+            </div>
+
+            <div data-color="neutral-900" data-mt="3" data-stack="y" data-cross="center"> 
+              <div data-fs="5xl">${weather.temperatureCelsius} °C</div>
+              <div data-fs="xl">odczuw. ${weather.feelsLikeCelsius} °C</div>
+            </div>
+          </div>
+
+          <div data-fs="xl" data-color="neutral-300" data-mt="8">${weather.precipitation.probability}% opadów (ok. ${weather.precipitation.hour}:00)</div>
+        </section>
+
+        <section data-color="neutral-400" data-m="8">
           Wygenerowano ${weather.generatedAt
             .toInstant()
             .toZonedDateTimeISO("Europe/Warsaw")
