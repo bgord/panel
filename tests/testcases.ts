ae1c0be7eaab04a4c407c308e9a0810246e7399e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-11 20:41:29 +0200

    Remove unused testcases file

--- tests/testcases.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "bun:test";
-
-export async function assertInvariantError(response: Response, code: number, message: string) {
-  const json = await response.json();
-
-  expect(response.status).toEqual(code);
-  expect(json).toEqual({ message, _known: true });
-}
