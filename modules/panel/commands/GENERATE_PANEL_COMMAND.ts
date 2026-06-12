726f4308269623e5868007d6418ea79f5ff4e73e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-12 13:23:57 +0200

    Fix circular dependencies

--- modules/panel/commands/GENERATE_PANEL_COMMAND.ts
+++ modules/panel/commands/GENERATE_PANEL_COMMAND.ts
@@ -1,7 +1,7 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
-import * as VO from "+panel/value-objects";
+import { PanelLocation } from "../value-objects/panel-location";
 
 // Stryker disable next-line StringLiteral
 export const GENERATE_PANEL_COMMAND = "GENERATE_PANEL_COMMAND";
@@ -9,7 +9,7 @@ export const GENERATE_PANEL_COMMAND = "GENERATE_PANEL_COMMAND";
 export const GeneratePanelCommand = v.object({
   ...bg.CommandEnvelopeSchema,
   name: v.literal(GENERATE_PANEL_COMMAND),
-  payload: v.object({ location: VO.PanelLocation, timezone: tools.Timezone, language: tools.Language }),
+  payload: v.object({ location: PanelLocation, timezone: tools.Timezone, language: tools.Language }),
 });
 
 export type GeneratePanelCommandType = v.InferOutput<typeof GeneratePanelCommand>;
