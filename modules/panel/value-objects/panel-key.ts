f44b2a15c0063442ac687b358fe2f545c0b19d35
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-12 13:35:51 +0200

    Fix circular dependency in panel key

--- modules/panel/value-objects/panel-key.ts
+++ modules/panel/value-objects/panel-key.ts
@@ -1,10 +1,10 @@
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
-import * as Panel from "+panel";
+import { PanelMime } from "./panel-mime-registry";
 
 export class PanelKeyFactory {
   static stable(language: tools.LanguageType) {
-    const filename = tools.Filename.fromParts(`panel-${language}`, Panel.VO.PanelMime.extensions[0]!);
+    const filename = tools.Filename.fromParts(`panel-${language}`, PanelMime.extensions[0]!);
 
     return v.parse(tools.ObjectKey, `panels/${filename.get()}`);
   }
