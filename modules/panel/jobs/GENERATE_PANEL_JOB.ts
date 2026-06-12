b4009645970b24706e2a1d34d93df91d25c57a79
Bartosz Gordon <gordonbartosz@gmail.com>
2026-06-12 13:38:56 +0200

    Fix circular dependency in generate panel job

--- modules/panel/jobs/GENERATE_PANEL_JOB.ts
+++ modules/panel/jobs/GENERATE_PANEL_JOB.ts
@@ -1,7 +1,7 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
-import * as Panel from "+panel";
+import { PanelLocation } from "../value-objects/panel-location";
 
 // Stryker disable next-line StringLiteral
 export const GENERATE_PANEL_JOB = "GENERATE_PANEL_JOB";
@@ -9,7 +9,7 @@ export const GENERATE_PANEL_JOB = "GENERATE_PANEL_JOB";
 export const GeneratePanelJobSchema = v.object({
   ...bg.JobEnvelopeSchema,
   name: v.literal(GENERATE_PANEL_JOB),
-  payload: v.object({ location: Panel.VO.PanelLocation, timezone: tools.Timezone, language: tools.Language }),
+  payload: v.object({ location: PanelLocation, timezone: tools.Timezone, language: tools.Language }),
 });
 
 export type GeneratePanelJobType = v.InferOutput<typeof GeneratePanelJobSchema>;
