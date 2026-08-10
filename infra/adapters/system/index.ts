c731af245b8b2f4ea6bb8e1377899ae4062dcc5e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-10 15:30:42 +0200

    Update packages

--- infra/adapters/system/index.ts
+++ infra/adapters/system/index.ts
@@ -60,6 +60,7 @@ export async function createSystemAdapters(Env: EnvironmentResultType) {
       HashFile,
       FileCleaner,
       FileRenamer,
+      FileInspection,
       NonceProvider,
       Logger,
       Clock,
